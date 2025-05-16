ad831f3d144a3b51a70ce7ee92cde58f195987b3
Agnes Nduta <47296541+AgnesNM@users.noreply.github.com>
2025-05-16 15:11:34 +0300

    Create joins_queries.sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,17 @@
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    u.phone_number
+FROM 
+    Booking b
+INNER JOIN 
+    [User] u ON b.user_id = u.user_id
+ORDER BY 
+    b.start_date;
